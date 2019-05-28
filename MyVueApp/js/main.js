7903487524c471ccb19c6205a837b72ac7911001
my0shym <benzenetarou@gmail.com>
2019-05-29 01:17:17 +0900

    chapter3

--- MyVueApp/js/main.js
+++ MyVueApp/js/main.js
@@ -5,7 +5,11 @@
     var vm = new Vue({
         el: '#app',
         data: {
-            name: 'taguchi'
+            todos: [
+                'task1',
+                'task2',
+                'task3'            
+            ]
         }
     })
 
