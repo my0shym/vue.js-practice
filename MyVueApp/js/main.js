5fbb36a09786de9d0ae7f340b8d0a27c06898cfe
my0shym <benzenetarou@gmail.com>
2019-05-29 22:58:31 +0900

    <chapter15> implement function to button

--- MyVueApp/js/main.js
+++ MyVueApp/js/main.js
@@ -4,7 +4,17 @@
     // Components
 
     var likeComponent = Vue.extend({
-        template: '<button>Like</button>'
+        data: function(){
+            return {
+                count: 0
+            }
+        },
+        template: '<button @click="countUp">Like {{ count }}</button>',
+        methods: {
+            countUp: function(){
+                this.count++;
+            }
+        },
     });
 
     var app = new Vue({
