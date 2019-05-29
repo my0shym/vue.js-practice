8ad6df94e18c9b9b246a35241f6c7cfb7d31ef8b
my0shym <benzenetarou@gmail.com>
2019-05-29 22:52:32 +0900

    <chapter14> reset and use Component

--- MyVueApp/js/main.js
+++ MyVueApp/js/main.js
@@ -1,51 +1,16 @@
 (function(){
     'use strict';
 
-    // two way data binding (to UI)
-    var vm = new Vue({
-        el: '#app',
-        data: {
-            newItem: '',
-            todos: []
-        },
-        watch: {
-           todos: {
-                handler: function(){
-                    localStorage.setItem('todos', JSON.stringify(this.todos));
-                },
-                deep: true
-            }
-        },
-        mounted: function(){
-            this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-        },
-        methods: {
-            addItem: function(){
-                var item = {
-                    title: this.newItem,
-                    isDone: false
-                };
-                this.todos.push(item);
-                this.newItem = '';
-            },
-            deleteItem: function(index){
-                if (confirm('are you sure?')){
-                    this.todos.splice(index, 1);
-                }
-            },
-            purge: function(){
-                if (!confirm('delete finished?')){
-                    reutrn;
-                }
-                this.todos = this.remainging;
-            },
-        },
-        computed: {
-            remainging: function(){
-                return this.todos.filter(function(todo){
-                    return !todo.isDone;
-                });
-            }
-        }
+    // Components
+
+    var likeComponent = Vue.extend({
+        template: '<button>Like</button>'
     });
+
+    var app = new Vue({
+        el: '#app',
+        components: {
+            'like-component': likeComponent
+        }
+    })
 })();
